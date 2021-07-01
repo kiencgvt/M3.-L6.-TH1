a31d3657cd814804fd67520ac7d02f3ce3b079ec
Your Name <you@example.com>
2021-07-01 11:48:14 +0700

    [Thực hành] Ứng dụng quản lý khách hàng: Quản lý tỉnh

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cities = City::all();
+        return view('cities.list', compact('cities'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cities.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $city = new City();
+        $city->name = $request->input('name');
+        $city->save();
+        Session::flash('success', 'Thêm mới tỉnh thành thành công');
+        return redirect()->route('cities.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $city = City::findOrFail($id);
+        return view('cities.edit', compact('city'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $city = City::findOrFail($id);
+        $city->name = $request->input('name');
+        $city->save();
+        Session::flash('success', 'Cập nhật tỉnh thành thành công');
+        return redirect()->route('cities.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $city = City::findOrFail($id);
+        $city->customers()->delete();
+        $city->delete();
+        Session::flash('success', 'Xóa tỉnh thành thành công');
+        return redirect()->route('cities.index');
+    }
+}
